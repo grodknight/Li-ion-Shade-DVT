744665ea0a5ef6614cb58111482276dfc7c12ed1
Gerard Knight Crestron <gknight@crestron.com>
2026-09-07 09:36:50 -0400

    Part of inital commt

---
+++ WS_SPI_PWM_CLI/spi_pwm_cli/cmake_gcc/build/CMakeFiles/3.30.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.19045")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.19045")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/svn/New-shade_spi-cli-mct/WS_SPI_PWM_CLI/spi_pwm_cli/cmake_gcc/toolchain.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ WS_SPI_PWM_CLI/spi_pwm_cli/cmake_gcc/build/generated/build_info.h
@@ -0,0 +1,6 @@
+#ifndef SPI_PWM_CLI_BUILD_INFO_H
+#define SPI_PWM_CLI_BUILD_INFO_H
+
+#define BUILD_NUMBER "38"
+
+#endif
